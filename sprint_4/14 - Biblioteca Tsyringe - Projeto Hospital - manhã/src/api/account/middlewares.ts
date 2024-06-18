f5a2eba6b235a811181d58bdd5f6ed8883d4efa2
Chrystian Rodolfo <chrystian@kenzie.com.br>
2024-06-18 10:50:38 -0300

    added m5-t21-sp4-demo2-tsyringe-manha

---
+++ sprint_4/14 - Biblioteca Tsyringe - Projeto Hospital - manhã/src/api/account/middlewares.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response, request, response } from "express";
+import { ForbiddenError } from "../@shared/errors";
+
+export function isAccountOwner(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const accountIdParam = req.params.id;
+
+  const { accountId } = res.locals;
+
+  if (accountIdParam !== accountId) {
+    throw new ForbiddenError();
+  }
+
+  return next();
+}
+
+export function isNurse(req: Request, res: Response, next: NextFunction) {
+  const { decodedToken } = res.locals;
+
+  // console.log("###################");
+  // console.log(decodedToken);
+  // console.log("###################");
+
+  // TODO: comparar com o enum diretamente
+  if (decodedToken.role !== "NURSE") {
+    throw new ForbiddenError();
+  }
+
+  req.body.createdById = Number(decodedToken.sub);
+
+  return next();
+}
