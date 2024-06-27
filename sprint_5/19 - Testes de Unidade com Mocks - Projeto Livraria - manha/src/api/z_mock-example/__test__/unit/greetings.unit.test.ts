28057f12af6ba4cbc20d5022b53cd9566986eb4e
Chrystian Rodolfo <chrystian@kenzie.com.br>
2024-06-27 11:59:52 -0300

    added m5-t21-sp5-demo3-testes-de-unidade-manha

---
+++ sprint_5/19 - Testes de Unidade com Mocks - Projeto Livraria - manha/src/api/z_mock-example/__test__/unit/greetings.unit.test.ts
@@ -0,0 +1,60 @@
+import { GreettingService } from "../../grettings";
+
+/*
+  EVENT LOOP
+  CALL STACK
+*/
+
+describe("UNIT EXEMPLO: GreetingService", () => {
+  let greetingService: GreettingService;
+
+  beforeEach(() => {
+    greetingService = new GreettingService();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("deve retornar `Bom dia!!` caso o horário atual seja anterior a 12", () => {
+    // AAA
+
+    // Arrange
+    // jest
+    //   .spyOn(greetingService, "getCurrentTime")
+    //   .mockReturnValue(new Date("1993-03-17T09:06:38.214Z"));
+    const mockGetCurrentTime = jest.spyOn(greetingService, "getCurrentTime");
+    mockGetCurrentTime.mockReturnValueOnce(
+      new Date("1993-03-17T09:06:38.214Z")
+    );
+
+    // Act
+    const result = greetingService.getGreeting();
+
+    // Assert
+    expect(result).toBe("Bom dia!!");
+    expect(greetingService.getCurrentTime).toHaveBeenCalledTimes(1);
+  });
+
+  test("deve retornar `Boa tarde!!` caso o horário atual esteja entre 12 e 18", () => {
+    jest
+      .spyOn(greetingService, "getCurrentTime")
+      .mockReturnValue(new Date("1993-03-17T16:06:38.214Z"));
+
+    const result = greetingService.getGreeting();
+
+    expect(result).toBe("Boa tarde!!");
+    expect(greetingService.getCurrentTime).toHaveBeenCalledTimes(1);
+  });
+
+  test("deve retornar `Boa noite` caso o horário atual for após 18", () => {
+    jest
+      .spyOn(greetingService, "getCurrentTime")
+      .mockReturnValue(new Date("1993-03-17T23:06:38.214Z"));
+
+    const result = greetingService.getGreeting();
+
+    expect(result).toBe("Boa noite!!");
+    expect(greetingService.getCurrentTime).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ sprint_5/19 - Testes de Unidade com Mocks - Projeto Livraria - manha/z_anotacoes/dia3.md
@@ -0,0 +1,9 @@
+# Dia 3
+
+Testes de unidade são os testes mais básicos e granulares. Tem como objetivo verificiar o comportamento de unidades individuais de código, como funções/métodos isoladamente do resto do sistema.
+
+TESTES DE UNIDADE NAO INTERAGEM COM BANCO
+
+Mocks
+
+Um mock é um objeto que simula o comportamento de objetos reais de maneira controlada durante os testes. São usados para substituir partes do sistema que estão sendo testadas.
