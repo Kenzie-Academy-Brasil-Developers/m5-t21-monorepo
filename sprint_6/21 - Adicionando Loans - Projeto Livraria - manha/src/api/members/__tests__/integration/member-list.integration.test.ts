313cd2b4ef28967e399503c1f09e050d811689ce
Chrystian Rodolfo <chrystian@kenzie.com.br>
2024-07-02 10:55:04 -0300

    added m5-t21-sp6-demo2-add-loans-manha

---
+++ sprint_6/21 - Adicionando Loans - Projeto Livraria - manha/src/api/members/__tests__/integration/member-list.integration.test.ts
@@ -0,0 +1,6 @@
+// TODO: Implementar os testes quando chegar a parte de Paginação
+describe("INTEGRATION: GET /api/members", () => {
+  test("should return an empty list when no members are available", () => {
+    expect(1).toBe(1);
+  });
+});
